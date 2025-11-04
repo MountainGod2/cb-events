91aaeb565ec512cd0ce8a9c696ca497ffbb00f55
MountainGod2 <admin@reid.ca>
2025-11-03 17:35:16 -0700

    refactor(client): remove unused client default configurations

--- src/cb_events/client.py
+++ src/cb_events/client.py
@@ -24,13 +24,6 @@ BASE_URL = "https://eventsapi.chaturbate.com/events"
 TESTBED_URL = "https://events.testbed.cb.dev/events"
 URL_TEMPLATE = "{base_url}/{username}/{token}/?timeout={timeout}"
 
-# Client defaults
-DEFAULT_TIMEOUT = 10
-DEFAULT_RETRY_ATTEMPTS = 8
-DEFAULT_RETRY_BACKOFF = 1.0
-DEFAULT_RETRY_FACTOR = 2.0
-DEFAULT_RETRY_MAX_DELAY = 30.0
-
 # Rate limiting
 RATE_LIMIT_MAX_RATE = 2000
 RATE_LIMIT_TIME_PERIOD = 60
